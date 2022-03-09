db919f34fb32daab21ff230244abf4f577a2ff5d
Polavarapu Hari Nagarjuna Chowdary <hari@Polavarapus-MacBook-Pro.local>
2022-03-09 08:44:40 +0530

    first commit

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,12 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwToken = Cookies.get('jwt_token')
+  if (jwToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
